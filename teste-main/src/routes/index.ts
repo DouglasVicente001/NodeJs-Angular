a2e8d16cf296467eb9c8af5bc9e99eccba6b8586
dsve <douglas.vicente@gft.com>
2022-09-04 12:51:26 -0300

    CrudsFinalizados2

--- teste-main/src/routes/index.ts
+++ teste-main/src/routes/index.ts
@@ -2,8 +2,7 @@ import { Router } from "express";
 import { atendimentoRoutes } from "./atendimento.routes";
 import { clienteRoutes } from "./cliente.routes";
 import { produtoRoutes } from "./produto.routes";
-// import { palavrasRoutes } from "./palavra.routes";
-// import { relacaoRoutes } from "./relacao.routes";
+
 import { usuarioRoutes } from "./usuario.routes";
 
 const routes = Router();
